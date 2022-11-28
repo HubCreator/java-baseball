e76197bebea980ce68cc6bb603977303b4d775e9
HubCreator <chocomilk8604@gmail.com>
2022-11-28 19:09:08 +0900

    refactor(ViewMessage) : 개행 문자 추가

--- src/main/java/baseball/enums/ViewMessage.java
+++ src/main/java/baseball/enums/ViewMessage.java
@@ -7,7 +7,7 @@ public enum ViewMessage {
     N_BALL("{0}볼 "),
     N_STRIKE("{0}스트라이크 "),
     NOTHING("낫싱 "),
-    END_MESSAGE(String.format("%d개의 숫자를 모두 맞히셨습니다! 게임 종료", ConstVariable.SIZE.getValue())),
+    END_MESSAGE(String.format("\n%d개의 숫자를 모두 맞히셨습니다! 게임 종료", ConstVariable.SIZE.getValue())),
     RETRY_OR_QUIT_MESSAGE(String.format("게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.", RetryOrQuit.RETRY.getValue(), RetryOrQuit.QUIT.getValue()));
 
     private final String value;
